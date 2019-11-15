709e475b0967333d1f828b448b433020045570a0
SergeiAvchinnikov <serge.avchinnikov@mail.ru>
2019-11-15 20:40:53 +0000

    Завершено упражнение с модулями

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,78 +1,27 @@
 terraform {
   # Версия terraform
-  required_version = "0.11.07"
+  required_version = "0.11.11"
 }
 
 provider "google" {
-  # Версия провайдера
-  version = "2.0.0"
-
-  # ID проекта
-  project = "${var.project}" #""
-  region  = "${var.region}"  #"europe-west-1"
+version = "2.0.0"
+project = "${var.project}"
+region = "${var.region}"
+}
+module "app" {
+source = "modules/app"
+public_key_path = "${var.public_key_path}"
+zone = "${var.zone}"
+app_disk_image = "${var.app_disk_image}"
+}
+module "db" {
+source = "modules/db"
+public_key_path = "${var.public_key_path}"
+zone = "${var.zone}"
+db_disk_image = "${var.db_disk_image}"
+}
+module "vpc" {
+source = "modules/vpc"
+source_ranges = ["0.0.0.0/0"]
 }
 
-resource "google_compute_instance" "app" {
-  name         = "reddit-app"
-  machine_type = "g1-small"
-  zone         = "${var.zone}"
-  tags         = ["reddit-app"]
-
-  # определение загрузочного диска
-  boot_disk {
-    initialize_params {
-      image = "${var.disk_image}" #"reddit-base"
-    }
-  }
-
-  # определение сетевого интерфейса
-  network_interface {
-    # сеть, к которой присоединить данный интерфейс
-    network = "default"
-
-    # использовать ephemeral IP для доступа из Интернет
-    access_config {}
-  }
-
-  metadata {
-    # путь до публичного ключа
-    ssh-keys = "appuser:${file(var.public_key_path)}"
-  }
-
-  connection {
-    type  = "ssh"
-    user  = "appuser"
-    agent = false
-
-    # путь до приватного ключа
-    private_key = "${file(var.public_key_path)}"
-  }
-
-  provisioner "file" {
-    source      = "files/puma.service"
-    destination = "/tmp/puma.service"
-  }
-
-  provisioner "remote-exec" {
-    script = "files/deploy.sh"
-  }
-}
-
-resource "google_compute_firewall" "firewall_puma" {
-  name = "allow-puma-default"
-
-  # Название сети, в которой действует правило
-  network = "default"
-
-  # Какой доступ разрешить
-  allow {
-    protocol = "tcp"
-    ports    = ["9292"]
-  }
-
-  # Каким адресам разрешаем доступ
-  source_ranges = ["0.0.0.0/0"]
-
-  # Правило применимо для инстансов с перечисленными тэгами
-  target_tags = ["reddit-app"]
-}
