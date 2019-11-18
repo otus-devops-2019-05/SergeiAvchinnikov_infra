fcbeefc0fc09c9eab0a4d5218d6b3514a5b6e9f8
SergeiAvchinnikov <52485773+SergeiAvchinnikov@users.noreply.github.com>
2019-11-18 10:35:07 +0300

    Terraform 2 (#6)
    * Завершено упражнение с модулями
    
    * Добавлены stage и prod
    
    * проверены stage и prod, удалены лишние файлы
    
    * добавлены примеры .tfvard в stage и prodd
    
    * добавлены под версиооный контроль файлы пакера, исправлена версия терраформ
    
    * fix-1
    
    * storage bucket
    
    * Обновлен readme

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,21 +8,3 @@ variable region {
   # Значение по умолчанию
   default = "europe-west1"
 }
-
-variable public_key_path {
-  # Описание переменной
-  description = "Path to the public key used for ssh access"
-}
-
-variable disk_image {
-  description = "Disk image"
-}
-
-variable private_key_path {
-  description = "Path to the private key"
-}
-
-variable zone {
-  description = "Zone"
-  default     = "europe-west1-b"
-}
